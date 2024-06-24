a6f23b0ecc54173849a79e5dc2f6fa42cbb8e53e
Michele <mikzed@hotmail.it>
2024-06-24 12:21:35 -0700

    project completed

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
-var Startup = /** @class */ (function () {
-    function Startup(nome, settoreDiFocus, descrizione, prodottiServiziOfferti) {
-        this.nome = nome;
-        this.settoreDiFocus = settoreDiFocus;
-        this.descrizione = descrizione;
-        this.prodottiServiziOfferti = prodottiServiziOfferti;
-    }
-    Startup.prototype.riceviIncentivo = function (incentivo) {
-        console.log("La startup ".concat(this.nome, " ha ricevuto l'incentivo ").concat(incentivo.descrizione));
-    };
-    return Startup;
-}());
-var Incentivo = /** @class */ (function () {
-    function Incentivo(codiceIdentificativo, descrizione, valore, criteriDiEleggibilita) {
-        this.codiceIdentificativo = codiceIdentificativo;
-        this.descrizione = descrizione;
-        this.valore = valore;
-        this.criteriDiEleggibilita = criteriDiEleggibilita;
-    }
-    Incentivo.prototype.assegnaAStartup = function (startup) {
-        console.log("L'incentivo ".concat(this.descrizione, " \u00E8 stato assegnato alla startup ").concat(startup.nome));
-    };
-    return Incentivo;
-}());
-var Cittadino = /** @class */ (function () {
-    function Cittadino(nome, cognome, eta, interessiSportivi) {
-        this.nome = nome;
-        this.cognome = cognome;
-        this.eta = eta;
-        this.interessiSportivi = interessiSportivi;
-    }
-    Cittadino.prototype.partecipaAttivita = function (startup) {
-        console.log("".concat(this.nome, " ").concat(this.cognome, " partecipa alle attivit\u00E0 della startup ").concat(startup.nome));
-    };
-    return Cittadino;
-}());
-var startup1 = new Startup("Sportech", "Tecnologie wearable", "Sviluppo di dispositivi indossabili per il monitoraggio delle prestazioni sportive", ["Smartwatch sportivi", "Sensori di attività"]);
-var startup2 = new Startup("FitApp", "App per il fitness", "Piattaforma mobile per la gestione e il monitoraggio dell'allenamento personale", ["App di fitness personalizzata"]);
-var incentivo1 = new Incentivo("INV001", "Incentivo per lo sviluppo di tecnologie sportive", 5000, ["Startup innovative nel settore sportivo"]);
-var incentivo2 = new Incentivo("INV002", "Incentivo per promuovere l'attività fisica nelle scuole", 3000, ["Scuole primarie e secondarie"]);
-var cittadino1 = new Cittadino("Mario", "Rossi", 35, ["Jogging", "Calcio"]);
+function IStartup(nome, settoreDiFocus, descrizione, prodottiServiziOfferti) {
+    this.nome = nome;
+    this.settoreDiFocus = settoreDiFocus;
+    this.descrizione = descrizione;
+    this.prodottiServiziOfferti = prodottiServiziOfferti;
+}
+IStartup.prototype.riceviIncentivo = function (incentivo) {
+    console.log("La startup ".concat(this.nome, " ha ricevuto l'incentivo ").concat(incentivo.descrizione));
+};
+function IIncentivo(codiceIdentificativo, descrizione, valore, criteriDiEleggibilita) {
+    this.codiceIdentificativo = codiceIdentificativo;
+    this.descrizione = descrizione;
+    this.valore = valore;
+    this.criteriDiEleggibilita = criteriDiEleggibilita;
+}
+IIncentivo.prototype.assegnaAStartup = function (startup) {
+    console.log("L'incentivo ".concat(this.descrizione, " \u00E8 stato assegnato alla startup ").concat(startup.nome));
+};
+function ICittadino(nome, cognome, eta, interessiSportivi) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.eta = eta;
+    this.interessiSportivi = interessiSportivi;
+}
+ICittadino.prototype.partecipaAttivita = function (startup) {
+    console.log("".concat(this.nome, " ").concat(this.cognome, " partecipa alle attivit\u00E0 della startup ").concat(startup.nome));
+};
+var startup1 = new IStartup("Sportech", "Tecnologie wearable", "Sviluppo di dispositivi indossabili per il monitoraggio delle prestazioni sportive", ["Smartwatch sportivi", "Sensori di attività"]);
+var startup2 = new IStartup("FitApp", "App per il fitness", "Piattaforma mobile per la gestione e il monitoraggio dell'allenamento personale", ["App di fitness personalizzata"]);
+var incentivo1 = new IIncentivo("INV001", "Incentivo per lo sviluppo di tecnologie sportive", 5000, ["Startup innovative nel settore sportivo"]);
+var incentivo2 = new IIncentivo("INV002", "Incentivo per promuovere l'attività fisica nelle scuole", 3000, ["Scuole primarie e secondarie"]);
+var cittadino1 = new ICittadino("Mario", "Rossi", 35, ["Jogging", "Calcio"]);
 incentivo1.assegnaAStartup(startup1);
 startup1.riceviIncentivo(incentivo1);
 cittadino1.partecipaAttivita(startup1);
--- app.ts
+++ app.ts
@@ -1,101 +1,76 @@
-interface IStartup {
-  nome: string;
-  settoreDiFocus: string;
-  descrizione: string;
-  prodottiServiziOfferti: string[];
-  riceviIncentivo(incentivo: IIncentivo): void;
+function IStartup(nome, settoreDiFocus, descrizione, prodottiServiziOfferti) {
+  this.nome = nome;
+  this.settoreDiFocus = settoreDiFocus;
+  this.descrizione = descrizione;
+  this.prodottiServiziOfferti = prodottiServiziOfferti;
 }
 
-interface IIncentivo {
-  codiceIdentificativo: string;
-  descrizione: string;
-  valore: number;
-  criteriDiEleggibilita: string[];
-  assegnaAStartup(startup: IStartup): void;
+IStartup.prototype.riceviIncentivo = function (incentivo) {
+  console.log(
+    `La startup ${this.nome} ha ricevuto l'incentivo ${incentivo.descrizione}`
+  );
+};
+
+function IIncentivo(
+  codiceIdentificativo,
+  descrizione,
+  valore,
+  criteriDiEleggibilita
+) {
+  this.codiceIdentificativo = codiceIdentificativo;
+  this.descrizione = descrizione;
+  this.valore = valore;
+  this.criteriDiEleggibilita = criteriDiEleggibilita;
 }
 
-interface ICittadino {
-  nome: string;
-  cognome: string;
-  eta: number;
-  interessiSportivi: string[];
-  partecipaAttivita(startup: IStartup): void;
+IIncentivo.prototype.assegnaAStartup = function (startup) {
+  console.log(
+    `L'incentivo ${this.descrizione} è stato assegnato alla startup ${startup.nome}`
+  );
+};
+
+function ICittadino(nome, cognome, eta, interessiSportivi) {
+  this.nome = nome;
+  this.cognome = cognome;
+  this.eta = eta;
+  this.interessiSportivi = interessiSportivi;
 }
 
-class Startup implements IStartup {
-  constructor(
-    public nome: string,
-    public settoreDiFocus: string,
-    public descrizione: string,
-    public prodottiServiziOfferti: string[]
-  ) {}
+ICittadino.prototype.partecipaAttivita = function (startup) {
+  console.log(
+    `${this.nome} ${this.cognome} partecipa alle attività della startup ${startup.nome}`
+  );
+};
 
-  riceviIncentivo(incentivo: IIncentivo): void {
-    console.log(
-      `La startup ${this.nome} ha ricevuto l'incentivo ${incentivo.descrizione}`
-    );
-  }
-}
-
-class Incentivo implements IIncentivo {
-  constructor(
-    public codiceIdentificativo: string,
-    public descrizione: string,
-    public valore: number,
-    public criteriDiEleggibilita: string[]
-  ) {}
-
-  assegnaAStartup(startup: IStartup): void {
-    console.log(
-      `L'incentivo ${this.descrizione} è stato assegnato alla startup ${startup.nome}`
-    );
-  }
-}
-
-class Cittadino implements ICittadino {
-  constructor(
-    public nome: string,
-    public cognome: string,
-    public eta: number,
-    public interessiSportivi: string[]
-  ) {}
-
-  partecipaAttivita(startup: IStartup): void {
-    console.log(
-      `${this.nome} ${this.cognome} partecipa alle attività della startup ${startup.nome}`
-    );
-  }
-}
-
-const startup1 = new Startup(
+const startup1 = new IStartup(
   "Sportech",
   "Tecnologie wearable",
   "Sviluppo di dispositivi indossabili per il monitoraggio delle prestazioni sportive",
   ["Smartwatch sportivi", "Sensori di attività"]
 );
 
-const startup2 = new Startup(
+const startup2 = new IStartup(
   "FitApp",
   "App per il fitness",
   "Piattaforma mobile per la gestione e il monitoraggio dell'allenamento personale",
   ["App di fitness personalizzata"]
 );
 
-const incentivo1 = new Incentivo(
+const incentivo1 = new IIncentivo(
   "INV001",
   "Incentivo per lo sviluppo di tecnologie sportive",
   5000,
   ["Startup innovative nel settore sportivo"]
 );
 
-const incentivo2 = new Incentivo(
+const incentivo2 = new IIncentivo(
   "INV002",
   "Incentivo per promuovere l'attività fisica nelle scuole",
   3000,
   ["Scuole primarie e secondarie"]
 );
 
-const cittadino1 = new Cittadino("Mario", "Rossi", 35, ["Jogging", "Calcio"]);
+const cittadino1 = new ICittadino("Mario", "Rossi", 35, ["Jogging", "Calcio"]);
 
 incentivo1.assegnaAStartup(startup1);
 startup1.riceviIncentivo(incentivo1);
